0867e254af1fdf25d81003f10469f6b42ab2ce48
Bishal Sarkar <69084883+bishalsarkar77730@users.noreply.github.com>
2020-09-07 11:01:29 +0530

    Add files via upload

---
+++ change files name in directory.py
@@ -0,0 +1,21 @@
+# importing os module
+import os
+
+
+# Function to rename multiple files
+def main():
+    for count, filename in enumerate(os.listdir("xyz")):
+        dst = "xyz" + str(count) + ".jpg"
+        src = 'xyz' + filename
+        dst = 'xyz' + dst
+
+        # rename() function will
+        # rename all the files
+        os.rename(src, dst)
+
+    # Driver Code
+
+
+if __name__ == '__main__':
+    # Calling main() function
+    main()
